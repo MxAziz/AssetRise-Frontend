61f299f9aee20b5f9ca0ecf898f4808786a3ac1d
MxAziz <hereabdulaziz@gmail.com>
2026-03-22 11:55:35 +0600

    added basic nav menu

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -2,11 +2,39 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav className="bg-gray-800 text-white p-4">
-
-            <div className="container mx-auto flex justify-between items-center">
-                <Link href="/" className="text-xl font-bold">AssetRise</Link>
-            </div>
-        </nav>
+      <nav className="pt-10 w-360 mx-auto">
+        <div className="container mx-auto flex justify-between items-center">
+          <div>
+            <Link href="/" className="text-xl font-bold">
+              AssetRise
+            </Link>
+          </div>
+          {/* route pages */}
+          <div>
+            <Link href="/" className="mr-4">
+              Home
+            </Link>
+            <Link href="/properties" className="mr-4">
+              Properties
+            </Link>
+            <Link href="/agents" className="mr-4">
+              Agents
+            </Link>
+            <Link href="/about" className="mr-4">
+              About
+            </Link>
+            <Link href="/blog" className="mr-4">
+              Blog
+            </Link>
+            <Link href="/contact" className="mr-4">
+              Contact
+            </Link>
+          </div>
+          {/* end button */}
+          <div>
+            <button>Book a Viewing</button>
+          </div>
+        </div>
+      </nav>
     );
 }
